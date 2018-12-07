fee4815b1b20e3ce660260170695db928955814a
John Habibi <johnhabibi@gmail.com>
2018-12-07 16:09:59 -0600

    More elm

--- Bingo.elm
+++ Bingo.elm
@@ -4,10 +4,52 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 
 
+
+-- MODEL
+
+
+type alias Model =
+    { name : String
+    , gameNumber : Int
+    , entries : List Entry
+    }
+
+
+type alias Entry =
+    { id : Int
+    , phrase : String
+    , points : Int
+    , marked : Bool
+    }
+
+
+initialModel : Model
+initialModel =
+    { entries = initialEntries
+    , gameNumber = 1
+    , name = "John"
+    }
+
+
+initialEntries : List Entry
+initialEntries =
+    [ Entry 1 "Future-Proof" 100 False
+    , Entry 2 "Doing Agile" 200 False
+    , Entry 3 "In The Cloud" 300 False
+    , Entry 4 "Rock-Star Ninja" 400 False
+    ]
+
+
+
+-- VIEW
+
+
+playerInfo : String -> Int -> String
 playerInfo name gameNumber =
     name ++ " - Game #" ++ toString gameNumber
 
 
+viewPlayer : String -> Int -> Html msg
 viewPlayer name gameNumber =
     let
         playerInfoText =
@@ -19,11 +61,13 @@ viewPlayer name gameNumber =
         [ playerInfoText ]
 
 
+viewHeader : String -> Html msg
 viewHeader title =
     header []
         [ h1 [] [ text title ] ]
 
 
+viewFooter : Html msg
 viewFooter =
     footer []
         [ a [ href "https://elm-lang.org" ]
@@ -31,13 +75,34 @@ viewFooter =
         ]
 
 
-view =
+viewEntryItem : Entry -> Html msg
+viewEntryItem entry =
+    li []
+        [ span [ class "phrase" ] [ text entry.phrase ]
+        , span [ class "points" ] [ text (toString entry.points) ]
+        ]
+
+
+viewEntryList : List Entry -> Html msg
+viewEntryList entries =
+    let
+        listOfEntries =
+            List.map viewEntryItem entries
+    in
+    ul [] listOfEntries
+
+
+view : Model -> Html msg
+view model =
     div [ class "content" ]
         [ viewHeader "BUZZWORD BINGO"
-        , viewPlayer "Mina" 4
+        , viewPlayer model.name model.gameNumber
+        , viewEntryList model.entries
+        , div [ class "debug" ] [ text (toString model) ]
         , viewFooter
         ]
 
 
+main : Html msg
 main =
-    view
+    view initialModel
